446790b5593f2ab8471998fd9c4e568686ca663e
RodrigoFaccioSales <118194746+RodrigoFaccioSales@users.noreply.github.com>
2023-03-05 09:50:53 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [buttonPosition, setButtonPosition] = useState({ x: '55%', y: '50%' });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+
+  const handleMouseMove = (event) => {
+    if (event.target === noButtonRef.current) {
+      const newX = Math.random() * (window.innerWidth - 100);
+      const newY = Math.random() * (window.innerHeight - 50);
+      setButtonPosition({ x: newX, y: newY });
+    }
+  };
+
+  const handleNoButtonClick = () => {
+    setButtonDisabled(true);
+  };
+
+  const noButtonRef = React.createRef();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <h1>Quer ser minha futura esposa?</h1>
+      <button
+        style={{ position: 'absolute', top:'50%', left:'50%', border:'none',backgroundColor:'green',width:50,height:30,borderRadius:8,color:'white' }}
+        onClick={() => alert('Você clicou em Sim!')}
+        disabled={buttonDisabled}
+      >
+        Sim
+      </button>
+      <button
+        ref={noButtonRef}
+        style={{ position: 'absolute', top: buttonPosition.y, left: buttonPosition.x,border:'none',backgroundColor:'red',width:50,height:30,borderRadius:8,color:'white'}}
+        onMouseMove={handleMouseMove}
+        onClick={handleNoButtonClick}
+      >
+        Não
+      </button>
     </div>
   );
 }
